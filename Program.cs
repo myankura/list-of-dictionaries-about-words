245a960d4fa9199dcae8b04ec9389ba72d633286
Michael Yankura <myankura210@gmail.com>
2019-05-29 13:17:29 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace list_of_dictionaries_about_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
